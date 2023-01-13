c4042318cb293897a5e8d787b728b259afa66712
Gianluca Moriconi <gianluca.moriconi@tiendanube.com>
2023-01-13 13:42:48 -0300

    Fix anchors for deploy

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -7,10 +7,10 @@ const BottomMenu = () => {
         <div id="bottom-menu" className="position-fixed shadow w-100 p-3 rounded bg-white bottom-0 ">
             <div className="d-flex flex-wrap w-100">
                 <div className="col-4 pe-3">
-                    <a className="btn-primary btn w-100">Para la tienda</a>
+                    <a href="/" className="btn-primary btn w-100">Para la tienda</a>
                 </div>
                 <div className="col-4 pe-3">
-                    <a className="btn-primary btn w-100">Para el checkout</a>
+                    <a href="/" className="btn-primary btn w-100">Para el checkout</a>
                 </div>
                 <div className="col-4">
                     <Link to="/result" className="btn btn-primary font-monospace fs-6 ms-4">&#60; Ver código &#62;</Link>
